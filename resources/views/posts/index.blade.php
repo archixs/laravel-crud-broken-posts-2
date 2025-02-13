da4caa2e9f8987b4f17d8d2746f23ca85e9d9a2a
Arturs <arturshugo4@gmail.com>
2025-02-13 10:57:18 +0200

    added laravel breeze + design

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,21 +1,45 @@
 <x-app-layout>
-    <h1>All posts</h1>
-    <a href="{{ route('create') }}">Create post</a>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('All posts') }}
+        </h2>
+    </x-slot>
+    
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <a href="{{ route('create') }}">Create post</a>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
     <ul>
         @foreach($allPosts as $post)
-            <li>
-                <h2>Title: {{ $post->title }}</h2>
-                <p>Content: {{ $post->content }}</p>
-                <div>
-                    <a href="{{ route('show', $post->id)}}">Show</a>
-                    <a href="{{ route('edit', $post->id)}}">Edit</a>
-                    <form action="{{ route('destroy', $post->id)}}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        <button>Delete</button>
-                    </form>
+        <div class="py-1">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        <li>
+                            <h2 class="font-bold text-2xl">{{ $post->title }}</h2>
+                            <p>{{ $post->content }}</p>
+                            <div>
+                                <a href="{{ route('show', $post->id)}}" class="btn btn-secondary">Show</a>
+                                <a href="{{ route('edit', $post->id)}}">Edit</a>
+                                <form action="{{ route('destroy', $post->id)}}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button>Delete</button>
+                                </form>
+                            </div>
+                        </li>
+                    </div>
                 </div>
-            </li>
+            </div>
+        </div>
+            
         @endforeach
     </ul>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,29 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;    
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
-use App\Models\Post;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('posts', [PostController::class, 'index'])->name('index');
-Route::get('posts/create', [PostController::class, 'create'])->name('create');
-Route::post('posts', [PostController::class, 'store'])->name('store');
-Route::get('posts/{id}/edit', [PostController::class, 'edit'])->name('edit');
-Route::put('posts/{id}/update', [PostController::class, 'update'])->name('update');
-Route::delete('posts/{id}/destroy', [PostController::class, 'destroy'])->name('destroy');
-Route::get('posts/{id}', [PostController::class, 'show'])->name('show');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('posts', [PostController::class, 'index'])->name('index');
+    Route::get('posts/create', [PostController::class, 'create'])->name('create');
+    Route::post('posts', [PostController::class, 'store'])->name('store');
+    Route::get('posts/{id}/edit', [PostController::class, 'edit'])->name('edit');
+    Route::put('posts/{id}/update', [PostController::class, 'update'])->name('update');
+    Route::delete('posts/{id}/destroy', [PostController::class, 'destroy'])->name('destroy');
+    Route::get('posts/{id}', [PostController::class, 'show'])->name('show');
+    });
+
+require __DIR__.'/auth.php';
